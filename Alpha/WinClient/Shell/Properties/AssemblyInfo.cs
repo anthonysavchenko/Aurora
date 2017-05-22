f70821830e79131b491141250692a4a1a0a9cbe4
Григорий Кречетов <greenz.gk@live.com>
2017-05-22 20:23:37 +1000

    Init

--- Alpha/WinClient/Shell/Properties/AssemblyInfo.cs
+++ Alpha/WinClient/Shell/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AuroraAssembly")]
-[assembly: AssemblyDescription("AuroraAssembly")]
+[assembly: AssemblyTitle("Aurora Капремонт")]
+[assembly: AssemblyDescription("Aurora Капремонт")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Taumis")]
 [assembly: AssemblyProduct("Aurora")]
