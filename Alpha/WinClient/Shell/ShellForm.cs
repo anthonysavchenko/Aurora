0238bf1656c350188b59cbf011738fbb5dc84ab5
Григорий Кречетов <greenz.gk@live.com>
2017-05-31 15:41:45 +1000

    Заголовок главного окна

--- Alpha/WinClient/Shell/ShellForm.cs
+++ Alpha/WinClient/Shell/ShellForm.cs
@@ -34,7 +34,7 @@ namespace Taumis.Infrastructure.Shell
             _statusWorkspace.Name = CommonWorkspaceNames.StatusWorkspace;
 			
 			this.IsMdiContainer = true;
-            this.Text = "Aurora 1871";
+            this.Text = "Aurora Капремонт";
         }
 
     }
