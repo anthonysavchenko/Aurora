be5ddad0c1b9eb9fe2bc69f9fe7c49abb223efd5
Григорий Кречетов <greenz.gk@live.com>
2017-10-02 11:09:59 +1000

    FIX запись долга в квитанцию при нулевых начислениях

--- Alpha/WinClient/Modules/Accounting/Charges/Views/Wizard/WizardViewPresenter.cs
+++ Alpha/WinClient/Modules/Accounting/Charges/Views/Wizard/WizardViewPresenter.cs
@@ -1172,8 +1172,7 @@ namespace Taumis.Alpha.WinClient.Aurora.Modules.Accounting.Charges.Views.Wizard
 
                                     #endregion
 
-                                    decimal _currentPeriodTotal = 0,
-                                            _rest = 0;
+                                    decimal _currentPeriodTotal = 0;
 
                                     if (_customerPeriodBalances.Balances.ContainsKey(_currentPeriod))
                                     {
@@ -1186,12 +1185,12 @@ namespace Taumis.Alpha.WinClient.Aurora.Modules.Accounting.Charges.Views.Wizard
 
                                         Balance _totalBalance = _customerPeriodBalances.Balances[_currentPeriod].TotalBalance;
                                         _currentPeriodTotal = _totalBalance.Charge + _totalBalance.Benefit + _totalBalance.Correction;
-
-                                        _rest =
-                                            _customerPeriodBalances.Balances
-                                                .Where(periodBalance => periodBalance.Key < _currentPeriod)
-                                                .Sum(periodBalance => periodBalance.Value.TotalBalance.Total);
                                     }
+
+                                    decimal _rest =
+                                        _customerPeriodBalances.Balances
+                                            .Where(periodBalance => periodBalance.Key < _currentPeriod)
+                                            .Sum(periodBalance => periodBalance.Value.TotalBalance.Total);
 
                                     #region Переплата
 
