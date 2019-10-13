12954ddda884daf2f05721740bb7122e154f595c
Anton Savchenko <anthony.savchenko@gmail.com>
2019-10-13 13:21:55 +1000

    Исправления по модификации лицевых счетов при загрузук файла льготников ДВОВ.

---
+++ Alpha/WinClient/Modules/Service/Import/Queries/GetNotActualBenefitAccountsSquareQuery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Taumis.Alpha.DataBase;
+using Taumis.Alpha.Infrastructure.Interface.Constants;
+
+namespace Taumis.Alpha.WinClient.Aurora.Modules.Service.Import.Queries
+{
+    public static class GetNotActualBenefitAccountsSquareQuery
+    {
+        public class SquareData
+        {
+            public Customers ParentCustomer { get; set; }
+            public decimal BenefitCustomerSquare { get; set; }
+        }
+
+        public static List<SquareData> GetNotActualBenefitAccountsSquare(
+            this Entities db,
+            int buildingId,
+            DateTime lastChargedPeriod) =>
+            db.Customers
+                .Where(x => x.Buildings.ID == buildingId
+                    && x.Residents.All(y => y.BenefitTypes.Code == BenefitTypeCodes.CHILDREN_OF_WAR)
+                    && x.CustomerPoses.Any(y => y.Till == lastChargedPeriod))
+                .Select(x =>
+                    new SquareData
+                    {
+                        ParentCustomer = db.Customers
+                            .Where(y => y.Buildings.ID == x.Buildings.ID
+                                && y.Apartment == x.Apartment
+                                && y.Residents.All(z => z.BenefitTypes == null || z.BenefitTypes.Code != BenefitTypeCodes.CHILDREN_OF_WAR)
+                                && y.CustomerPoses.Any(z => z.Till > lastChargedPeriod))
+                            .FirstOrDefault(),
+                        BenefitCustomerSquare = x.Square,
+                    })
+                .Where(x => x.ParentCustomer != null)
+                .ToList();
+    }
+}
---
+++ Alpha/WinClient/Modules/Service/Import/Queries/GetResidentsOfNotActualFullShareBenefitAccountsQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Taumis.Alpha.DataBase;
+using Taumis.Alpha.Infrastructure.Interface.Constants;
+
+namespace Taumis.Alpha.WinClient.Aurora.Modules.Service.Import.Queries
+{
+    public static class GetResidentsOfNotActualFullShareBenefitAccountsQuery
+    {
+        public static List<Residents> GetResidentsOfNotActualFullShareBenefitAccounts(
+            this Entities db,
+            int buildingId,
+            List<string> actualAppartments,
+            DateTime firstUnchargedPeriod) =>
+            db.Residents
+                .Where(x => x.Customers.Buildings.ID == buildingId
+                    && !actualAppartments.Contains(x.Customers.Apartment)
+                    && x.BenefitTypes.Code == BenefitTypeCodes.CHILDREN_OF_WAR
+                    && x.Customers.CustomerPoses.Any(y => y.Till >= firstUnchargedPeriod))
+                .ToList();
+    }
+}
