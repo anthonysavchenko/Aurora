fb9426e2a91b26e91d9907609440555edcad9a28
Anton Savchenko <anthony.savchenko@gmail.com>
2021-07-02 15:00:45 +1000

    Возможность скрывать старые услуги у абонента.

---
+++ Alpha/Infrastructure/SQLServerAccessProvider/Queries/GetNowDateTimeQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Taumis.Alpha.DataBase;
+
+namespace Taumis.Alpha.Infrastructure.SQLAccessProvider.Queries
+{
+    public static class GetNowDateTimeQuery
+    {
+        public static DateTime GetNowDateTime(this Entities db)
+        {
+            return db.CreateQuery<DateTime>("CurrentDateTime()").AsEnumerable().First();
+        }
+    }
+}
