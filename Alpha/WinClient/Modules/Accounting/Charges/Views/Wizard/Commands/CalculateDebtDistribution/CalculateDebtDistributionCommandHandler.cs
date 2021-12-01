56c350f9b77bd3484ce1b6e20cfad4add5d84ecd
Anthony Savchenko <anthony.savchenko@gmail.com>
2021-12-01 21:00:52 +1000

    Внесение выявленной задолженности по одной услуге

--- Alpha/WinClient/Modules/Accounting/Charges/Views/Wizard/Commands/CalculateDebtDistribution/CalculateDebtDistributionCommandHandler.cs
+++ Alpha/WinClient/Modules/Accounting/Charges/Views/Wizard/Commands/CalculateDebtDistribution/CalculateDebtDistributionCommandHandler.cs
@@ -16,43 +16,15 @@ namespace Taumis.Alpha.WinClient.Aurora.Modules.Accounting.Charges.Views.Wizard.
 
         public void Execute(CalculateDebtDistributionCommand command)
         {
-            var _calculateChargeCommand =
-                    new CalculateChargeCommand
-                    {
-                        CustomerInfo = command.CustomerInfo,
-                        Cache = command.Cache
-                    };
-            _dispatcher.Execute(_calculateChargeCommand);
-
-            decimal _distributionSum = _calculateChargeCommand.Result.Sum(x => x.Value);
-
-            command.Result = new Dictionary<int, decimal>();
-
-            if (_distributionSum > 0)
+            foreach (var c in command.CustomerInfo.Poses)
             {
-                decimal _coefficient = command.DebtValue / _distributionSum;
-
-                _distributionSum = 0;
-
-                foreach (var _posValuePair in _calculateChargeCommand.Result)
+                if (c.ServiceId == 52)
                 {
-                    decimal _value = _coefficient * _posValuePair.Value;
-
-                    _distributionSum += _value;
-
-                    if (_distributionSum > command.DebtValue)
+                    if (command.Result == null)
                     {
-                        _value -= _distributionSum - command.DebtValue;
-                        _distributionSum = command.DebtValue;
+                        command.Result = new Dictionary<int, decimal>();
                     }
-
-                    command.Result.Add(_posValuePair.Key, Math.Round(_value, 2, MidpointRounding.AwayFromZero));
-                }
-
-                if (_distributionSum < command.DebtValue)
-                {
-                    command.Result[command.Result.First().Key] += command.DebtValue - _distributionSum;
-                    _distributionSum = command.DebtValue;
+                    command.Result.Add(c.Id, Math.Round(command.DebtValue, 2, MidpointRounding.AwayFromZero));
                 }
             }
         }
