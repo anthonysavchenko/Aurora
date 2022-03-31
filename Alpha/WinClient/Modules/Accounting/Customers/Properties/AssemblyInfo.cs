f086219df3fd324a79a91f6cfa7e9f81031820ac
Anthony Savchenko <anthony.savchenko@gmail.com>
2022-03-31 16:51:20 +1000

    Исправил ошибку билда про CultureName

--- Alpha/WinClient/Modules/Accounting/Customers/Properties/AssemblyInfo.cs
+++ Alpha/WinClient/Modules/Accounting/Customers/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Aurora")]
 [assembly: AssemblyCopyright("Copyright © Taumis")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("ru-RU")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
